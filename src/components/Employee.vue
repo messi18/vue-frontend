c209a3aef7bbdcc7d541871f5fb864a247e383d7
lance <lance3a@gmail.com>
2022-07-10 18:10:04 +0800

    finish demo code

---
+++ src/components/Employee.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <h1 class="text-center">Employee List</h1>    
+        <table class="table table-striped">
+            <thead>
+                <th>Employee Id</th>
+                <th>Employee First Name</th>
+                <th>Employee Last Name</th>
+            </thead>
+            <tbody>
+                <tr v-for="employee in employee_list" v-bind:key="employee.Id">
+                    <td> {{employee.id}}</td>
+                    <td> {{employee.firstName}}</td>
+                    <td> {{employee.lastName}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>    
+</template>
+<script>
+    import EmployeeService from '../service/EmployeeService'
+    export default {
+        name:'employee_list',
+        data() {
+            return {
+                employee_list:[]
+            }
+        },
+        methods:{
+            getEmployees() {
+                EmployeeService.getEmployees().then((response) => {
+                    this.employee_list = response.data;
+                })
+            }
+        },
+        created() {
+            this.getEmployees()
+        }
+    }
+</script>
